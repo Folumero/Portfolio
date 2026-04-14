83a1fbdd0c859d2fb47747ca549f162f2c00eb63
Folumero <costacostadiego@gmail.com>
2026-04-14 20:52:24 +0200

    Steam

---
+++ SteamConnect/GameInstanceFinal.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AdvancedFriendsGameInstance.h"
+#include "GameInstanceFinal.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DC_MULTIJUGADOR_API UGameInstanceFinal : public UAdvancedFriendsGameInstance
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void CreateAdvancedSessionCPlusPlus(int32 PublicConnections = 4, int32 PrivateConnections = 0);
+
+
+private:
+	void OnCreateSessionComplete(FName SessionName, bool bWasSuccessful);
+
+	IOnlineSessionPtr SessionInterface;
+	FDelegateHandle CreateSessionDelegateHandle;
+	TSharedPtr<FOnlineSessionSettings> SessionSettings;
+};
